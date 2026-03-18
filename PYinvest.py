a9defc0dfb809ca8cabe325e9c2600ba8175bea7
matheusgarcia0711-ctrl <matheusgarcia0711@gmail.com>
2026-03-18 19:14:50 -0300

    Update PYinvest.py

--- PYinvest.py
+++ PYinvest.py
@@ -4,32 +4,34 @@ import datetime
 import statistics
 import locale
 
-locale.setlocal(locale.LC_ALL, 'pt_BR.UTF-8')
- ENTRADAS
- capital = float(input( 'Capital inicial: '))
-aporte  = float(input(( 'Prazo mensal')))
-meses  = float (INPUT('pRAZO (MESES): '))
-cdi_anual = float (input( 'CDI anual %:  '))/100
-perc_lci = float (input( 'Percentual do CDI - CDB (%): '))/100
-perc_lci = float(input( 'Percentual do CDI - LCI (%): '))
-taxa_fii = float(input('Rentabilidade do FII (%):'))/100
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+# ENTRADAS
+capital = float(input('Capital inicial: '))
+aporte  = float(input('Aporte mensal: '))
+meses  = float(input('Prazo (meses): '))
+cdi_anual = float(input('CDI anual %: ')) / 100
+perc_cdb = float(input('Percentual do CDI - CDB (%): ')) / 100
+perc_lci = float(input('Percentual do CDI - LCI (%): ')) / 100
+taxa_fii = float(input('Rentabilidade do FII (%): ')) / 100
 meta = float(input('Meta financeira (R$): '))
 
-#conversao CDI
-cdi_mensal = math.pow((1+cdi_anual), 1/12) -1
-#total investido
+# conversao CDI
+cdi_mensal = math.pow((1 + cdi_anual), 1/12) - 1
+
+# total investido
 total_investido = capital + (aporte * meses)
 
-#CDB
+# CDB
 taxa_cdb = cdi_mensal * perc_cdb
-montante_cdb = capital * math.pow((1+taxa_cdb),meses))+(aporte * meses)
-lucro_cdb = montante_cdb -  total_investido
-montante_cdb_liquido = total_investido + (lucro_cdb * 0.05)
+montante_cdb = (capital * math.pow((1 + taxa_cdb), meses)) + (aporte * meses)
+lucro_cdb = montante_cdb - total_investido
+montante_cdb_liquido = total_investido + (lucro_cdb * 0.85)
 
-#LCI/LCI
+# LCI
 taxa_lci = cdi_mensal * perc_lci
-montante_lci = (capital * math.pow((1+taxa_lci),meses))+(aporte * meses)
+montante_lci = (capital * math.pow((1 + taxa_lci), meses)) + (aporte * meses)
 
-#poupança
+# poupanca
 taxa_poupanca = 0.005
-montante_poupanca= (capital * math.pow((1+taxa_poupanca),meses))+ (aporte*meses)
+montante_poupanca = (capital * math.pow((1 + taxa_poupanca), meses)) + (aporte * meses)
